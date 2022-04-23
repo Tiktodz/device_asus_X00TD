e467186617fcfbc67b942b1ff8aabdd8a2ec55fc
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-04-24 01:56:21 +0700

    X00TD: yaap choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X00TD.mk
+    $(LOCAL_DIR)/yaap_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X00TD-userdebug \
-    aosp_X00TD-eng \
-    aosp_X00TD-user
+    yaap_X00TD-userdebug \
+    yaap_X00TD-eng \
+    yaap_X00TD-user
 
