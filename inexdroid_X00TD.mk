0a9626837b1fab120744dd80914878e0fd75a6a7
Meisie Widuri <kuplemarkeple@gmail.com>
2023-10-28 17:14:45 +0700

    Update and rename blaze_X00TD.mk to inexdroid_X00TD.mk

--- inexdroid_X00TD.mk
+++ inexdroid_X00TD.mk
@@ -9,19 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Blaze stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/inexdroid/config/common.mk)
 
-# Blaze official flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := fakedotkit
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_X00TD
+PRODUCT_NAME := inexdroid_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
