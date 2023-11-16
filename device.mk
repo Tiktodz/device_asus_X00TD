7e4a06cb0ff4e9469f1b009ec5bb9832c8496518
dotkit <ewprjkt@proton.me>
2023-11-16 15:51:12 +0000

    X00TD: xd. Bringup
    Change-Id: I064e271130ef541f32363366115f6c4514888de9
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- device.mk
+++ device.mk
@@ -13,7 +13,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-xdroid
 
 # Ramdisk
 PRODUCT_PACKAGES += \
