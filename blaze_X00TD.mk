30e54b04fafde9c65c1c9db866d459c415dc2fb8
dotkit <ewprjkt@proton.me>
2023-12-31 16:06:55 +0000

    X00TD: Blaze to U
    Change-Id: I8930f8b1d6eb0f8035118d9a97b766ee78ef0edb

--- blaze_X00TD.mk
+++ blaze_X00TD.mk
@@ -16,6 +16,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 BLAZE_BUILD_TYPE := OFFICIAL
 BLAZE_MAINTAINER := fakedotkit
+WITH_GAPPS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -32,7 +33,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230805.001 10316531 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 14 UQ1A.231205.015 11084887 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:14/UQ1A.231205.015/11084887:user/release-keys"
