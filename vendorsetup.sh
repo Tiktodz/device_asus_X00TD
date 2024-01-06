eb7187ce502e61400e4251986c48150db2783d8b
dotkit <ewprjkt@proton.me>
2024-01-06 10:06:52 +0000

    X00TD: Add vendorsetup.sh for jenkins
    Signed-off-by: dotkit <ewprjkt@proton.me>

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+rm -rf vendor/asus
+rm -rf device/asus/sdm660-common
+
+git clone https://github.com/Tiktodz/android_device_asus_sdm660-common device/asus/sdm660-common
+git clone --single-branch https://github.com/Tiktodz/android_vendor_asus -b 14 vendor/asus
+git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660-4.19 kernel/asus/sdm660
+
