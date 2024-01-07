ca476a072e91ed821fbc9820fb6bdd308e85cf52
dotkit <ewprjkt@proton.me>
2024-01-07 12:42:29 +0000

    X00TD: Change kernel tree
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,5 +4,5 @@ rm -rf hardware/qcom-caf/bengal
 
 git clone https://github.com/Tiktodz/android_device_asus_sdm660-common device/asus/sdm660-common
 git clone --single-branch https://github.com/Tiktodz/android_vendor_asus -b 14 vendor/asus
-git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660-4.19 kernel/asus/sdm660
+git clone https://github.com/Tiktodz/android_kernel_asus_sdm660 kernel/asus/sdm660
 
