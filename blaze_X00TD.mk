77232ea7286416464ba44a1b227d3832e1772d54
Meisie Widuri <kuplemarkeple@gmail.com>
2024-01-11 19:42:29 +0700

    X00TD: TARGET_GAPPS_ARCH := arm64

--- blaze_X00TD.mk
+++ blaze_X00TD.mk
@@ -17,6 +17,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 BLAZE_BUILD_TYPE := OFFICIAL
 BLAZE_MAINTAINER := fakedotkit
 WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
