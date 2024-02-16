f1b1cb82a9841a245041c38f90130b9946b00148
Meisie Widuri <kuplemarkeple@gmail.com>
2024-02-16 23:52:23 +0700

    X00TD: Update and rename blaze_X00TD.mk to lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -9,20 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Blaze stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Blaze official flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := fakedotkit
-WITH_GAPPS := true
+#TARGET_FACE_UNLOCK_SUPPORTED := true
+#BLAZE_BUILD_TYPE := OFFICIAL
+#BLAZE_MAINTAINER := fakedotkit
+#WITH_GAPPS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_X00TD
+PRODUCT_NAME := lineage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -33,7 +33,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 14 UQ1A.240105.002 11129216 release-keys"
+    PRIVATE_BUILD_DESC="shiba-user 14 UQ1A.240205.004 11269751 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:14/UQ1A.240105.002/11129216:user/release-keys"
+BUILD_FINGERPRINT := "google/shiba/shiba:14/UQ1A.240205.004/11269751:user/release-keys"
