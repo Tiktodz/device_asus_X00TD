e036bb37586ce680e3c42579ed8db51cc9d7b23b
Meisie Widuri <kuplemarkeple@gmail.com>
2024-02-16 23:52:59 +0700

    X00TD: Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-rm -rf vendor/asus
-rm -rf device/asus/sdm660-common
-rm -rf hardware/qcom-caf/bengal
-
-git clone https://github.com/Tiktodz/android_device_asus_sdm660-common device/asus/sdm660-common
-git clone --single-branch https://github.com/Tiktodz/android_vendor_asus -b 14 vendor/asus
-git clone https://github.com/Tiktodz/android_kernel_asus_sdm660 kernel/asus/sdm660
-
