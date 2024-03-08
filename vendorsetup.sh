5b96f0f027f6255c7c08eeddf4ab761ba532fc19
Meisie Widuri <kuplemarkeple@gmail.com>
2024-03-08 22:24:18 +0700

    X00TD: Update vendorsetup.sh
    source updated

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 rm -rf vendor/asus
 rm -rf device/asus/sdm660-common
 rm -rf kernel/asus/sdm660
-rm -rf device/qcom/sepolicy-legacy-um
+#rm -rf device/qcom/sepolicy-legacy-um
 
 git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common -b udc device/asus/sdm660-common
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b tom/u kernel/asus/sdm660
 git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus -b udc vendor/asus
-git clone --depth=1 https://github.com/Tiktodz/android_device_qcom_sepolicy -b 14 device/qcom/sepolicy-legacy-um
+#git clone --depth=1 https://github.com/Tiktodz/android_device_qcom_sepolicy -b 14 device/qcom/sepolicy-legacy-um
 export TZ=Asia/Jakarta
