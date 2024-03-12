7cff88d76d7cb3a9fc83a05921f88576084d8b35
dotkit <ewprjkt@proton.me>
2024-03-12 07:39:59 +0000

    X00TD: UptoU
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- evolution_X00TD.mk
+++ evolution_X00TD.mk
@@ -9,13 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# EvolutionX Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := false
+TARGET_USES_MINI_GAPPS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := evolution_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -26,7 +32,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="husky-user-14-UQ1A.240205.004-11269751-release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/husky/husky:14/UQ1A.240205.004/11269751:user/release-keys"
+BUILD_FINGERPRINT := "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+rm -rf kernel/asus/sdm660
+rm -rf device/asus/sdm660-common
+rm -rf vendor/asus
+
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b udc kernel/asus/sdm660
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common -b udc device/asus/sdm660-common
+git clone --depth=1 https://github.com/Tiktodz/device_asus_X00TD -b eas device/asus/X00TD
+git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus -b udc vendor/asus
+
+export TZ=Asia/Jakarta
