b37bf0ec632dbc2200ccaca61a9c1798b8fc8732
Meisie Widuri <kuplemarkeple@gmail.com>
2024-03-15 17:37:51 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 rm -rf device/asus/sdm660-common
-rm -rf device/asus/X00TD
+#rm -rf device/asus/X00TD
 rm -rf kernel/asus/sdm660
 rm -rf vendor/asus
 
-git clone --depth=1 https://github.com/Tiktodz/device_asus_X00TD -b blaze device/asus/X00TD
-git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common -b blaze device/asus/sdm660-common
+#git clone --depth=1 https://github.com/Tiktodz/device_asus_X00TD -b blaze device/asus/X00TD
+git clone --depth=1 https://github.com/ProjectBlaze-Devices/device_asus_sdm660-common device/asus/sdm660-common
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b sdm660 kernel/asus/sdm660
-git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus -b udc vendor/asus
+git clone --depth=1 https://github.com/ProjectBlaze-Devices/android_vendor_asus vendor/asus
 
 export TZ=Asia/Jakarta
