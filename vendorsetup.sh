2f868b811d54435ed350f2ae07aa0cbc3f5ecf41
Meisie Widuri <kuplemarkeple@gmail.com>
2024-03-18 21:36:26 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,5 @@ rm -rf vendor/asus
 git clone --depth=1 --recursive https://github.com/RyuujiX/android_kernel_asus_sdm660 -b hmp-test kernel/asus/sdm660
 git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common -b 11 device/asus/sdm660-common
 git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus -b 11 vendor/asus
-
+git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton-clang
 export TZ=Asia/Jakarta
