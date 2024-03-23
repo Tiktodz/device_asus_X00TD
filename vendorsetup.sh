a662f82c5e6a0b696b4fb9e18760fa9210433208
Meisie Widuri <kuplemarkeple@gmail.com>
2024-03-23 15:34:15 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,9 +3,10 @@ rm -rf device/asus/sdm660-common
 rm -rf vendor/asus
 #rm -rf packages/modules/Bluetooth
 #rm -rf prebuilts/clang/host/linux-x86
-rm -rf packages/modules/Gki
+#rm -rf packages/modules/Gki
 
 git clone --depth=1 --recursive https://github.com/RyuujiX/android_kernel_asus_sdm660 -b hmp-test kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
 git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common -b 11 device/asus/sdm660-common
 git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus -b 11 vendor/asus
 #git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton-clang
