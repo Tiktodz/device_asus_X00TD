33fb25c665f17d16a2cb64c6f55e45227aea2fdd
Meisie Widuri <kuplemarkeple@gmail.com>
2024-03-25 18:47:01 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,11 +3,12 @@ rm -rf device/asus/sdm660-common
 rm -rf kernel/asus/sdm660
 rm -rf vendor/asus
 rm -rf hardware/interfaces
+rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
 
 #git clone --depth=1 https://github.com/Tiktodz/device_asus_X00TD -b blaze device/asus/X00TD
 git clone --depth=1 https://github.com/ProjectBlaze-Devices/device_asus_sdm660-common device/asus/sdm660-common
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b sdm660 kernel/asus/sdm660
 git clone --depth=1 https://github.com/ProjectBlaze-Devices/android_vendor_asus vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/hardware_interfaces hardware/interfaces
-
+git clone --depth=1 https://github.com/LineageOS/android_vendor_nxp_opensource_packages_apps_Nfc vendor/nxp/opensource/commonsys/packages/apps/Nfc
 export TZ=Asia/Jakarta
